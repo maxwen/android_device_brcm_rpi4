b2da05c36171e14f2be69fe759e10942d980777b
maxwen <max.weninger@gmail.com>
2020-07-28 22:26:21 +0200

    scripts: rework and add options

--- scripts/mk-raspi64-large-image.sh
+++ scripts/mk-raspi64-large-image.sh
@@ -1,26 +1,94 @@
 #!/bin/sh
 
-OUT_DIR_ROOT=$OUT_DIR/target/product/rpi4/
-OUT_IMAGE_FILE=$HOME/raspberrypi/omni.img
+#IN_IMAGE_DIR=$OUT_DIR/target/product/rpi4/
+#IN_BOOT_FILES=$ANDROID_BUILD_TOP/vendor/brcm/rpi4/proprietary/boot/
+#OUT_IMAGE_FILE=$HOME/raspberrypi/omni.img
 
-if  [ ! -f "$OUT_DIR_ROOT/system.img" ]; then
-    echo "no system.img"
+options=$(getopt -o ho:i:b: -- "$@")
+[ $? -eq 0 ] || { 
+    echo "Incorrect options provided"
+    exit 1
+}
+eval set -- "$options"
+while true; do
+    case "$1" in
+    -o)
+        shift
+        OUT_IMAGE_FILE=$1
+        shift
+        ;;
+    -i)
+        shift
+        IN_IMAGE_DIR=$1
+        shift
+        ;;
+    -b)
+        shift
+        IN_BOOT_FILES=$1
+        shift
+        ;;
+    -h)
+        echo "-i <image folder> -b <boot file dir> -o <image file>"
+        echo "e.g. -i $OUT_DIR/target/product/rpi4/ -b $ANDROID_BUILD_TOP/vendor/brcm/rpi4/proprietary/boot/ -o /tmp/omni.img"
+        exit 0
+        ;;
+    --)
+        shift
+        break
+        ;;
+    esac
+done
+
+if [ -z $IN_IMAGE_DIR ]; then
+    echo "missing -i <image folder>"
     exit 0
 fi
 
-if  [ ! -f "$OUT_DIR_ROOT/vendor.img" ]; then
-    echo "no vendor.img"
+if [ -z $OUT_IMAGE_FILE ]; then
+    echo "missing -o <image file>"
     exit 0
 fi
 
-if  [ ! -f "$OUT_DIR_ROOT/obj/KERNEL_OBJ/arch/arm64/boot/Image" ]; then
-    echo "no Image"
+if [ -z $IN_BOOT_FILES ]; then
+    echo "missing -b <boot file dir>"
     exit 0
 fi
 
-rm $OUT_IMAGE_FILE
+if  [ ! -f "$IN_IMAGE_DIR/system.img" ]; then
+    echo "no <input folder>/system.img"
+    exit 0
+fi
+
+if  [ ! -f "$IN_IMAGE_DIR/vendor.img" ]; then
+    echo "no <input folder>/vendor.img"
+    exit 0
+fi
+
+if  [ ! -f "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/Image" ]; then
+    echo "no <input folder>/obj/KERNEL_OBJ/arch/arm64/boot/Image"
+    exit 0
+fi
+
+if  [ ! -d $IN_BOOT_FILES ]; then
+    echo "no <boot file dir>"
+    exit 0
+fi
+
+if  [ ! -f "$IN_BOOT_FILES/config.txt" ]; then
+    echo "no <boot file dir>/config.txt"
+    exit 0
+fi
+
+echo "create: images $IN_IMAGE_DIR + boot files $IN_BOOT_FILES -> $OUT_IMAGE_FILE"
+
+if [ -f $OUT_IMAGE_FILE ]; then
+    rm $OUT_IMAGE_FILE
+fi
+
+echo "create empty image"
 dd if=/dev/zero of="$OUT_IMAGE_FILE" bs=1M count=5120
 
+echo "create partitions"
 sudo sfdisk "$OUT_IMAGE_FILE"  << EOF
 2,262144,0xC,*
 264192,4194304,0x83,-
@@ -28,28 +96,38 @@ sudo sfdisk "$OUT_IMAGE_FILE"  << EOF
 4982784,,-
 EOF
 
+echo "mount partitions"
 sudo kpartx -av "$OUT_IMAGE_FILE"
+
+echo "create file systems"
 sudo mkfs.vfat /dev/mapper/loop0p1 -n boot
 sudo mkfs.ext4 /dev/mapper/loop0p2 -L system
 sudo mkfs.ext4 /dev/mapper/loop0p3 -L vendor
 sudo mkfs.ext4 /dev/mapper/loop0p4 -L userdata
 
+echo "write system.img"
+sudo dd if="$IN_IMAGE_DIR/system.img" of=/dev/mapper/loop0p2 bs=1M
+echo "write vendor.img"
+sudo dd if="$IN_IMAGE_DIR/vendor.img" of=/dev/mapper/loop0p3 bs=1M
 
-sudo dd if="$OUT_DIR_ROOT/system.img" of=/dev/mapper/loop0p2 bs=1M
-sudo dd if="$OUT_DIR_ROOT/vendor.img" of=/dev/mapper/loop0p3 bs=1M
-
+echo "write boot patition"
 sudo mkdir /mnt/tmp
 sudo mount /dev/mapper/loop0p1 /mnt/tmp
-sudo cp "$OUT_DIR_ROOT/ramdisk.img" /mnt/tmp/
-sudo cp "$OUT_DIR_ROOT/obj/KERNEL_OBJ/arch/arm64/boot/Image" /mnt/tmp/Image
-sudo cp "$OUT_DIR_ROOT/obj/KERNEL_OBJ/arch/arm64/boot/dts/broadcom/bcm2711-rpi-4-b.dtb" /mnt/tmp/
+sudo cp "$IN_IMAGE_DIR/ramdisk.img" /mnt/tmp/
+sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/Image" /mnt/tmp/Image
+sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/broadcom/bcm2711-rpi-4-b.dtb" /mnt/tmp/
 sudo mkdir /mnt/tmp/overlays/
-sudo cp "$OUT_DIR_ROOT/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/vc4-kms-v3d-pi4.dtbo" /mnt/tmp/overlays/
-sudo cp "$OUT_DIR_ROOT/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/dwc2.dtbo" /mnt/tmp/overlays/
-sudo cp vendor/brcm/rpi4/proprietary/boot/* /mnt/tmp/
+sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/vc4-kms-v3d-pi4.dtbo" /mnt/tmp/overlays/
+sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/dwc2.dtbo" /mnt/tmp/overlays/
+sudo cp $IN_BOOT_FILES/* /mnt/tmp/
 sync
+
+echo "unmounting"
 sudo umount /mnt/tmp
 sudo rm -fr /mnt/tmp
 
 sudo kpartx -dv "$OUT_IMAGE_FILE"
+
+echo "now write $OUT_IMAGE_FILE to a sdcard"
+
 exit 1
--- scripts/update-raspi64-image.sh
+++ scripts/update-raspi64-image.sh
@@ -1,35 +1,102 @@
 #!/bin/sh
 
-OUT_DIR_ROOT=$OUT_DIR/target/product/rpi4/
+#IN_IMAGE_DIR=$OUT_DIR/target/product/rpi4/
+#IN_BOOT_FILES=$ANDROID_BUILD_TOP/vendor/brcm/rpi4/proprietary/boot/
+#SD_CARD_DEVICE=/dev/sdc
 
-if  [ ! -f "$OUT_DIR_ROOT/system.img" ]; then
-    echo "no system.img"
+options=$(getopt -o hi:d:b: -- "$@")
+[ $? -eq 0 ] || { 
+    echo "Incorrect options provided"
+    exit 1
+}
+eval set -- "$options"
+while true; do
+    case "$1" in
+    -i)
+        shift
+        IN_IMAGE_DIR=$1
+        shift
+        ;;
+    -d)
+        shift
+        SD_CARD_DEVICE=$1
+        shift
+        ;;
+    -b)
+        shift
+        IN_BOOT_FILES=$1
+        shift
+        ;;
+    -h)
+        echo "-i <input folder> -b <boot file dir> -d <sdcard device>"
+        echo "e.g. -i $OUT_DIR/target/product/rpi4/ -b $ANDROID_BUILD_TOP/vendor/brcm/rpi4/proprietary/boot/ -d /dev/sdc"
+        exit 0
+        ;;
+    --)
+        shift
+        break
+        ;;
+    esac
+done
+
+if [ -z $IN_IMAGE_DIR ]; then
+    echo "missing -i <input folder>"
     exit 0
 fi
 
-if  [ ! -f "$OUT_DIR_ROOT/vendor.img" ]; then
-    echo "no vendor.img"
+if [ -z $SD_CARD_DEVICE ]; then
+    echo "missing -d <sdcard device> e.g. /dev/sdc"
     exit 0
 fi
 
-if  [ ! -f "$OUT_DIR_ROOT/obj/KERNEL_OBJ/arch/arm64/boot/Image" ]; then
-    echo "no Image"
+if  [ ! -f "$IN_IMAGE_DIR/system.img" ]; then
+    echo "no <input folder>/system.img"
     exit 0
 fi
 
+if  [ ! -f "$IN_IMAGE_DIR/vendor.img" ]; then
+    echo "no <input folder>/vendor.img"
+    exit 0
+fi
+
+if  [ ! -f "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/Image" ]; then
+    echo "no <input folder>/obj/KERNEL_OBJ/arch/arm64/boot/Image"
+    exit 0
+fi
+
+if [ ! -b $SD_CARD_DEVICE ]; then
+    echo "no <sdcard device>"
+    exit 0
+fi
+
+if  [ ! -d $IN_BOOT_FILES ]; then
+    echo "no <boot file dir>"
+    exit 0
+fi
+
+if  [ ! -f "$IN_BOOT_FILES/config.txt" ]; then
+    echo "no <boot file dir>/config.txt"
+    exit 0
+fi
+
+echo "update: images $IN_IMAGE_DIR + boot files $IN_BOOT_FILES -> $SD_CARD_DEVICE"
+
+echo update boot partition on $SD_CARD_DEVICE"1"
 sudo mkdir /mnt/tmp
-sudo mount /dev/sdc1 /mnt/tmp
-sudo cp "$OUT_DIR_ROOT/ramdisk.img" /mnt/tmp
-sudo cp "$OUT_DIR_ROOT/obj/KERNEL_OBJ/arch/arm64/boot/Image" /mnt/tmp/Image
-sudo cp "$OUT_DIR_ROOT/obj/KERNEL_OBJ/arch/arm64/boot/dts/broadcom/bcm2711-rpi-4-b.dtb" /mnt/tmp
+sudo mount $SD_CARD_DEVICE"1" /mnt/tmp
+sudo cp "$IN_IMAGE_DIR/ramdisk.img" /mnt/tmp
+sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/Image" /mnt/tmp/Image
+sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/broadcom/bcm2711-rpi-4-b.dtb" /mnt/tmp
 sudo mkdir /mnt/tmp/overlays/
-sudo cp "$OUT_DIR_ROOT/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/vc4-kms-v3d-pi4.dtbo" /mnt/tmp/overlays/
-sudo cp "$OUT_DIR_ROOT/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/dwc2.dtbo" /mnt/tmp/overlays/
-sudo cp vendor/brcm/rpi4/proprietary/boot/* /mnt/tmp
+sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/vc4-kms-v3d-pi4.dtbo" /mnt/tmp/overlays/
+sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/dwc2.dtbo" /mnt/tmp/overlays/
+sudo cp $IN_BOOT_FILES/* /mnt/tmp/
 sync
 
-sudo dd if=$OUT_DIR_ROOT/system.img of=/dev/sdc2 bs=1M
-sudo dd if=$OUT_DIR_ROOT/vendor.img of=/dev/sdc3 bs=1M
+echo write system.img to $SD_CARD_DEVICE"2"
+sudo dd if=$IN_IMAGE_DIR/system.img of=$SD_CARD_DEVICE"2" bs=1M
+echo write vendor.img to $SD_CARD_DEVICE"3"
+sudo dd if=$IN_IMAGE_DIR/vendor.img of=$SD_CARD_DEVICE"3" bs=1M
 
 sudo umount /mnt/tmp
 sudo rm -fr /mnt/tmp
