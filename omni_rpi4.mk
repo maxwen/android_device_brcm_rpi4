e6ed23533cea14e1adfccec0266b6ace5de699cf
maxwen <max.weninger@gmail.com>
2020-09-02 14:34:27 +0200

    rpi4: add BOARD_INCLUDE_CMDLINE_TOOLS=true

--- omni_rpi4.mk
+++ omni_rpi4.mk
@@ -17,6 +17,8 @@
 # Inherit device configuration
 DEVICE_PATH := device/brcm/rpi4
 
+BOARD_INCLUDE_CMDLINE_TOOLS := true
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
