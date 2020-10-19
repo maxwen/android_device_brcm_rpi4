ee085cd925f08e179c397c1eb00837dbff7675f5
maxwen <max.weninger@gmail.com>
2020-10-19 10:02:27 +0200

    scripts: add -n option to skip boot partitoon on update

--- scripts/update-raspi64-image.sh
+++ scripts/update-raspi64-image.sh
@@ -3,13 +3,14 @@
 #IN_IMAGE_DIR=$OUT_DIR/target/product/rpi4/
 #IN_BOOT_FILES=$ANDROID_BUILD_TOP/vendor/brcm/rpi4/proprietary/boot/
 #SD_CARD_DEVICE=/dev/sdc
+#UPDATE_BOOT_PARTITION=1
 
 if [ -z $ROM_BUILDTYPE ]; then
     echo "missing ROM_BUILDTYPE"
     exit 0
 fi
 
-options=$(getopt -o hi:d:b: -- "$@")
+options=$(getopt -o nhi:d:b: -- "$@")
 [ $? -eq 0 ] || { 
     echo "Incorrect options provided"
     exit 1
@@ -32,9 +33,14 @@ while true; do
         IN_BOOT_FILES=$1
         shift
         ;;
+    -n)
+        UPDATE_BOOT_PARTITION=0
+        shift
+        ;;
     -h)
-        echo "-i <input folder> -b <boot file dir> -d <sdcard device>"
+        echo "-i <input folder> -b <boot file dir> -d <sdcard device> -n"
         echo "e.g. -i $OUT_DIR/target/product/rpi4/ -b $ANDROID_BUILD_TOP/vendor/brcm/rpi4/proprietary/boot/ -d /dev/sdc"
+        echo "-n dont update boot partition"
         exit 0
         ;;
     --)
@@ -86,25 +92,27 @@ fi
 
 echo "update: images $IN_IMAGE_DIR + boot files $IN_BOOT_FILES -> $SD_CARD_DEVICE"
 
-echo update boot partition on $SD_CARD_DEVICE"1"
-sudo mkdir /mnt/tmp
-sudo mount $SD_CARD_DEVICE"1" /mnt/tmp
-sudo cp "$IN_IMAGE_DIR/ramdisk.img" /mnt/tmp
-sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/Image" /mnt/tmp/Image
-sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/broadcom/bcm2711-rpi-4-b.dtb" /mnt/tmp
-sudo mkdir /mnt/tmp/overlays/
-sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/vc4-kms-v3d-pi4.dtbo" /mnt/tmp/overlays/
-sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/dwc2.dtbo" /mnt/tmp/overlays/
-sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/rpi-android-sdcard.dtbo" /mnt/tmp/overlays/
-sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/rpi-android-usb.dtbo" /mnt/tmp/overlays/
-sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/rpi-backlight.dtbo" /mnt/tmp/overlays/
-sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/rpi-ft5406.dtbo" /mnt/tmp/overlays/
-sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/ads7846.dtbo" /mnt/tmp/overlays/
-sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/gpio-key.dtbo" /mnt/tmp/overlays/
-sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/gpio-ir.dtbo" /mnt/tmp/overlays/
-sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/i2c-rtc.dtbo" /mnt/tmp/overlays/
-sudo cp $IN_BOOT_FILES/* /mnt/tmp/
-sync
+if [ $UPDATE_BOOT_PARTITION -eq 1 ]; then
+    echo update boot partition on $SD_CARD_DEVICE"1"
+    sudo mkdir /mnt/tmp
+    sudo mount $SD_CARD_DEVICE"1" /mnt/tmp
+    sudo cp "$IN_IMAGE_DIR/ramdisk.img" /mnt/tmp
+    sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/Image" /mnt/tmp/Image
+    sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/broadcom/bcm2711-rpi-4-b.dtb" /mnt/tmp
+    sudo mkdir /mnt/tmp/overlays/
+    sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/vc4-kms-v3d-pi4.dtbo" /mnt/tmp/overlays/
+    sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/dwc2.dtbo" /mnt/tmp/overlays/
+    sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/rpi-android-sdcard.dtbo" /mnt/tmp/overlays/
+    sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/rpi-android-usb.dtbo" /mnt/tmp/overlays/
+    sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/rpi-backlight.dtbo" /mnt/tmp/overlays/
+    sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/rpi-ft5406.dtbo" /mnt/tmp/overlays/
+    sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/ads7846.dtbo" /mnt/tmp/overlays/
+    sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/gpio-key.dtbo" /mnt/tmp/overlays/
+    sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/gpio-ir.dtbo" /mnt/tmp/overlays/
+    sudo cp "$IN_IMAGE_DIR/obj/KERNEL_OBJ/arch/arm64/boot/dts/overlays/i2c-rtc.dtbo" /mnt/tmp/overlays/
+    sudo cp $IN_BOOT_FILES/* /mnt/tmp/
+    sync
+fi
 
 echo write system.img to $SD_CARD_DEVICE"2"
 sudo dd if=$IN_IMAGE_DIR/system.img of=$SD_CARD_DEVICE"2" bs=1M
