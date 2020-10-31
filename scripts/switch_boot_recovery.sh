6d3b57265a327320029a78b798490ce8e01b7fea
maxwen <max.weninger@gmail.com>
2020-10-31 17:08:06 +0100

    scripts: add boot switch scripts
    from rom to recovery - /system/xbin/switch_boot.sh recovery (adb shell)
    from recovery to rom - /sbin/switch_boot_recovery.sh rom (adb shell)
    or flash /sbin/switch_boot_rom.zip

---
+++ scripts/switch_boot_recovery.sh
@@ -0,0 +1,29 @@
+#!/sbin/sh
+# called from recovery
+
+if [ -u $1 ]; then
+    # shortcut called from flashable zip
+    CONFIG_FILE_SOURCE=config.txt.rom
+fi
+
+CONFIG_FILE_TARGET=config.txt
+
+if [ $1 == "rom" ]; then
+    CONFIG_FILE_SOURCE=config.txt.rom
+elif [ $1 == "recovery" ]; then
+    CONFIG_FILE_SOURCE=config.txt.twrp
+else
+    echo "will switch to rom"
+fi
+
+if [ -b "/dev/block/mmcblk0p1" ]; then
+    mount /dev/block/mmcblk0p1 /boot
+    cp /boot/$CONFIG_FILE_SOURCE /boot/$CONFIG_FILE_TARGET
+    umount /boot
+    #/system/bin/reboot
+elif [ -b "/dev/block/sda1" ]; then
+    mount /dev/block/sda1 /boot
+    cp /boot/$CONFIG_FILE_SOURCE /boot/$CONFIG_FILE_TARGET
+    umount /boot
+    #/system/bin/reboot
+fi
